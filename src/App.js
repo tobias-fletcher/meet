4f824029da5b12d09ecbdceafe85e4161e92babc
tobias-fletcher <tobias_fletcher@icloud.com>
2021-07-28 00:01:59 -0500

    updated chart

--- src/App.js
+++ src/App.js
@@ -104,15 +104,17 @@ class App extends Component {
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEventCount={this.updateEventCount} />
         <h4>Events in each city</h4>
-        <ResponsiveContainer height={400} >
-          <ScatterChart height={400} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-            <CartesianGrid />
-            <XAxis dataKey="city" name="city" type="category" />
-            <YAxis dataKey="number" name="numbert" type="number" />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
+        <div className='.data-vis-wrapper'>
+          <ResponsiveContainer height={400} >
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+              <CartesianGrid />
+              <XAxis dataKey="city" name="city" type="category" />
+              <YAxis dataKey="number" name="numbert" type="number" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList events={this.state.events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => { getAccessToken() }} />
