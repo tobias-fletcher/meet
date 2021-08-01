e155d89612e04e4d9a0e0665c5834a3b1f9a84cb
tobias-fletcher <tobias_fletcher@icloud.com>
2021-08-01 10:16:24 -0500

    changed dark mode

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -3,6 +3,6 @@ import { createGlobalStyle } from "styled-components"
 export const GlobalStyles = createGlobalStyle`
   body {
     background-color: ${props => props.theme.mode === 'dark' ? '#111' : '#EEE'};
-    color: ${props => props.theme.mode === 'dark' ? '#EEE' : '#111'};
+    color: ${props => props.theme.mode === 'dark' ? '#A020F0' : '#111'};
   }
   `
